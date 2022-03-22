2c9d9b9121aca975f992bf0f49c7722da102a191
Adrien Carpentier <moi@adriencarpentier.com>
2022-03-22 11:56:46 +0900

    Add slowapi rate limiter, add twitter account profile picture asset

--- api.py
+++ api.py
@@ -3,14 +3,19 @@ import random
 import nltk
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 from common.generate_word import generate_word_and_save
 from config import ALLOW_ORIGINS
 from en.generate_definition import generate_definition_en
 from models import GenereratedWordEN, database
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
-
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOW_ORIGINS,
@@ -37,7 +42,8 @@ async def shutdown() -> None:
 
 
 @app.get("/{lang}/generate")
-async def generate(lang: str, request: Request):
+@limiter.limit("20/minute")
+async def generate(request: Request, lang: str):
     if lang not in ["en", "fr", "it", "es"]:
         raise HTTPException(status_code=400, detail="Language not supported.")
     ip: str = request.client.host
@@ -49,7 +55,8 @@ async def generate(lang: str, request: Request):
 
 
 @app.get("/{lang}/get")
-async def get_word_from_db(lang: str):
+@limiter.limit("20/minute")
+async def get_word_from_db(request: Request, lang: str):
     if lang not in ["en", "fr"]:
         raise HTTPException(status_code=400, detail="Language not supported.")
     if lang == "en":
@@ -59,9 +66,9 @@ async def get_word_from_db(lang: str):
     word = random.choice(list(words))
     return word
 
-
 @app.get("/{lang}/definition")
-async def get_definition(lang: str):
+@limiter.limit("5/minute")
+async def get_definition(request: Request, lang: str):
     if lang == "en":
         return await generate_definition_en()
     else:
