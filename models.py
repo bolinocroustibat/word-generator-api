8eeabfb3be3dd2ee3e145612a09b55255e788f4d
Adrien Carpentier <moi@adriencarpentier.com>
2022-03-25 00:01:01 +0900

    WIP

--- models.py
+++ models.py
@@ -1,19 +1,25 @@
-import databases
 import ormar
-import sqlalchemy
+from typing import Optional
 
+from sqlmodel import Field, SQLModel
 from config import MYSQL_URL
 
-metadata = sqlalchemy.MetaData()
-database = databases.Database(MYSQL_URL)
+# metadata = sqlalchemy.MetaData()
+# database = databases.Database(MYSQL_URL)
 
 
-class RealWordEN(ormar.Model):
-    id: int = ormar.Integer(primary_key=True)
-    string: str = ormar.String(max_length=50)
-    type: str = ormar.String(max_length=16, nullable=True)
-    number: str = ormar.String(max_length=1, nullable=True)
-    tense: str = ormar.String(max_length=16, nullable=True)
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+
+class RealWordEN(SQLModel):
+    id: int = Field(primary_key=True)
+    string: str = Field(max_length=50)
+    type: Optional[str] = Field(max_length=16, nullable=True)
+    number: Optional[str] = Field(max_length=1, nullable=True)
+    tense: Optional[str] = Field(max_length=16, nullable=True)
 
     class Meta:
         tablename = "real_words_EN"
@@ -22,13 +28,13 @@ class RealWordEN(ormar.Model):
         constraints = [ormar.UniqueColumns("string")]
 
 
-class RealWordFR(ormar.Model):
+class RealWordFR(SQLModel):
     id: int = ormar.Integer(primary_key=True)
-    string: str = ormar.String(max_length=100)
-    type: str = ormar.String(max_length=16, nullable=True)
-    gender: str = ormar.String(max_length=1, nullable=True)
-    number: str = ormar.String(max_length=1, nullable=True)
-    tense: str = ormar.String(max_length=16, nullable=True)
+    string: str = Field(max_length=100)
+    type: str = Field(max_length=16, nullable=True)
+    gender: str = Field(max_length=1, nullable=True)
+    number: str = Field(max_length=1, nullable=True)
+    tense: str = Field(max_length=16, nullable=True)
     proper: bool = ormar.Boolean()
     complex: bool = ormar.Boolean(nullable=True)
 
@@ -39,14 +45,14 @@ class RealWordFR(ormar.Model):
         constraints = [ormar.UniqueColumns("string", "type")]
 
 
-class GeneratedWordEN(ormar.Model):
+class GeneratedWordEN(SQLModel):
     id: int = ormar.Integer(primary_key=True)
-    string: str = ormar.String(max_length=100)
-    type: str = ormar.String(max_length=16, nullable=True)
-    number: str = ormar.String(max_length=1, nullable=True)
-    tense: str = ormar.String(max_length=16, nullable=True)
+    string: str = Field(max_length=100)
+    type: str = Field(max_length=16, nullable=True)
+    number: str = Field(max_length=1, nullable=True)
+    tense: str = Field(max_length=16, nullable=True)
     date = ormar.DateTime()
-    ip: str = ormar.String(max_length=16)
+    ip: str = Field(max_length=16)
 
     class Meta:
         tablename = "generated_words_EN"
@@ -55,16 +61,16 @@ class GeneratedWordEN(ormar.Model):
         constraints = [ormar.UniqueColumns("string")]
 
 
-class GeneratedWordFR(ormar.Model):
+class GeneratedWordFR(SQLModel):
     id: int = ormar.Integer(primary_key=True)
-    string: str = ormar.String(max_length=100)
-    type: str = ormar.String(max_length=16, nullable=True)
-    gender: str = ormar.String(max_length=1, nullable=True)
-    number: str = ormar.String(max_length=1, nullable=True)
-    tense: str = ormar.String(max_length=16, nullable=True)
-    conjug: str = ormar.String(max_length=16, nullable=True)
+    string: str = Field(max_length=100)
+    type: str = Field(max_length=16, nullable=True)
+    gender: str = Field(max_length=1, nullable=True)
+    number: str = Field(max_length=1, nullable=True)
+    tense: str = Field(max_length=16, nullable=True)
+    conjug: str = Field(max_length=16, nullable=True)
     date = ormar.DateTime()
-    ip: str = ormar.String(max_length=16)
+    ip: str = Field(max_length=16)
 
     class Meta:
         tablename = "generated_words_FR"
