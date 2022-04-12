e7270d876bc6e085e1240020f70590605788efd5
Adrien Carpentier <moi@adriencarpentier.com>
2022-04-12 22:46:36 +0900

    Fix tweet EN and prepare for tweet FR

---
+++ tweet_fr.py
@@ -0,0 +1,9 @@
+import asyncio
+
+from common.tweet import send_tweet
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    coroutine = send_tweet("fr")
+    generated: dict = loop.run_until_complete(coroutine)
